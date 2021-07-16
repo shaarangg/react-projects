556b84de78c73ebed5b56b7ec69bcc88ad1d01c8
shaarangg <shaaranggsingh@gmail.com>
2021-07-16 22:05:30 +0530

    feat: Finished a project named tours

--- 2-tours-fetch-data/src/App.js
+++ 2-tours-fetch-data/src/App.js
@@ -1,22 +1,56 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
 
 function App() {
+  const url = 'https://course-api.com/react-tours-project';
+  const [loading, setLoading] = useState(true);
+
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const reponse = await fetch(url);
+      const tours = await reponse.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(true);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <h2>no tours left</h2>
+        <button className="btn" onClick={fetchTours}>Refresh</button>
+      </main>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
+
 export default App;
