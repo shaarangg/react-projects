690f89067fb3b1deeb7416d44857f3d9675b5c57
shaarangg <shaaranggsingh@gmail.com>
2021-07-21 22:43:45 +0530

    feat: Made a restaurant menu

---
+++ 5-menu/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Menu from './Menu';
+import Categories from './Categories';
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))]
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newCategory = items.filter((item) => item.category === category);
+    setMenuItems(newCategory);
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
+}
+export default App;
