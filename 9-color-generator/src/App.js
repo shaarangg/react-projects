639267c08f6914fccc61e77bf423dbdf9f6f91c3
shaarangg <shaaranggsingh@gmail.com>
2021-07-24 20:01:03 +0530

    init: Initialized the project

---
+++ 9-color-generator/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import Values from 'values.js';
+import SingleColor from './SingleColor';
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+
+  }
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(e) => { setColor(e.target.value) }} placeholder="#f15025" className={`${error ? "error" : null}`} />
+          <button type="submit" className="btn">Hello</button>
+        </form>
+      </section>
+      <section className="colors">
+        {
+          list.map((color, index) => {
+            return <SingleColor key={index} {...color} index={index} />
+          })
+        }
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ 9-color-generator/src/SingleColor.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+function SingleColor({ rgb, weight, index }) {
+    return <div>Hello</div>
+}
+export default SingleColor;
