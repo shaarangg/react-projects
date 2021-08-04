2e323a67c0f045c977ff56b8e9829e557585fcfd
shaarangg <shaaranggsingh@gmail.com>
2021-08-04 19:37:43 +0530

    feat: Color generator

--- 9-color-generator/src/App.js
+++ 9-color-generator/src/App.js
@@ -1,37 +1,26 @@
 import React, { useState } from 'react';
 
 import Values from 'values.js';
-import SingleColor from './SingleColor';
+
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values("#f12050").all(10));
+  const [list, setList] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    try {
-      let colors = new Values(color).all(10);
-      setList(colors);
-    } catch (error) {
-      setError(true)
-      console.log(error)
-    }
-
+    console.log("Hello")
   }
   return (
     <>
       <section className="container">
         <h3>color generator</h3>
         <form action="" onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e) => { setColor(e.target.value) }} placeholder="#f12050" className={`${error ? "error" : null}`} />
-          <button type="submit" className="btn">generate</button>
+          <input type="text" value={color} onChange={(e) => { setColor(e.target.value) }} />
+          <button type="submit" className="btn">submit</button>
         </form>
       </section>
       <section className="colors">
-        {
-          list.map((color, index) => {
-            return <SingleColor key={index} {...color} hexColor={color.hex} index={index} />
-          })
-        }
+        Here color list goes
       </section>
     </>
   );
