f09701744219d5bca77e812a77328b857bf40e19
Shaarang Singh <shaarangg.singh@gmail.com>
2021-11-05 16:22:20 +0530

    Init: Inital commit

---
+++ 14-cart/src/context.js
@@ -0,0 +1,11 @@
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+	const [cart, setCart] = useState(cartItems);
+	return <AppContext.Provider value={{ cart }}>{children}</AppContext.Provider>;
+};
+export const useGlobalContext = () => {
+	return useContext(AppContext);
+};
+export { AppContext, AppProvider };
